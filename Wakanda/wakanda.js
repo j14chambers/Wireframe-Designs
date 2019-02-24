04214446a288d6acf9e3a0c0f8d9902a43182fba
j14chambers <jessica14chambers@gmail.com>
2019-02-24 15:28:47 -0600

    Move variables outside of function and added arrays -alphabet and wakandaLetters

--- Wakanda/wakanda.js
+++ Wakanda/wakanda.js
@@ -1,15 +1,19 @@
-function nameGenerator(){
+var $inputName = document.getElementById('input').value;
+    // var userName = document.getElementsByClassName("input-field")[0].value;
+    //console.log(document.querySelector('.input-field').value);
 
-    var userName = document.getElementsByClassName("input-field")[0].value;
-    console.log(document.getElementsByClassName("input-field")[0].value);
-
-    console.log(document.querySelector('.input-field').value);
-
-	$("button").click(function(){ 
+    $("button").click(function(){ 
         console.log('clicked');
-        console.log('Name: ' + userName);
+        console.log('$inputName: ' + $inputName);
     
     });
-	
-}
-nameGenerator();
+nameGenerator($inputName);
+
+function nameGenerator(wakandaName){
+
+    var alphabet = ['a', 'b','c', 'd','e','f','g','h','i', 'j', 'k', 'l','m','n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x','y', 'z'];
+
+    var wakandaLetters = ['ka', 'zu', 'mi', 'te', 'ku', 'lu', 'ji', 'ri', 'ki','zu', 'me', 'ta', 'rin', 'to', 'mo', 'no','ke', 'shi', 'ari', 'chi', 'do','ru', 'mei', 'na', 'fu', 'z'];
+
+};
+
